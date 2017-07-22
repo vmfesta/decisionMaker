f56096e43487e6ace0a2461a087c5c4e9d21e758
vmfesta <vmfesta@gmail.com>
2017-07-22 10:48:10 -0400

    addding server

--- server.js
+++ server.js
@@ -16,9 +16,7 @@ const knexLogger       = require("knex-logger");
 
 // Seperated Routes for each Resource
 const indexRoutes      = require("./routes/index");
-const poolRoutes = require("./routes/poolpage");
 const adminRoutes      = require("./routes/admin");
-const resultRoutes     = require("./routes/result");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -43,7 +41,7 @@ app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/index", indexRoutes(knex));
-app.use("/api/submission", indexRoutes(knex));
+app.use("/api/admin", adminRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
@@ -54,9 +52,9 @@ app.get("/", (req, res) => {
 //   res.render("pollpage");
 // });
 
-// app.get("/admin", (req, res) => {
-//   res.render("admin");
-// });
+app.get("/admin", (req, res) => {
+  res.render("admin");
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
